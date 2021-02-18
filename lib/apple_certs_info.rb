5e3886892e1695179711febd6419f152018fdc8d
tarappo <tarappo@gmail.com>
2021-02-18 15:30:46 +0900

    refactoring / not change api spec

--- lib/apple_certs_info.rb
+++ lib/apple_certs_info.rb
@@ -15,76 +15,61 @@ module AppleCertsInfo
 
   # Check Certificate file for iPhone Developer /Apple Development in the KeyChain
   # @param days: limit days
+  # @return:
+  #   expire_datetime: deadline
+  #   limit_days: limit days
+  #   cname: CN
   def self.certificate_development_list_limit_days_for(days:)
     raise "do not set days param" if days.nil?
-    limit_days_for(days: days, type: "certificate_development")
+    filtering_limit_days_for(list: certificate_development_list.uniq, days: days)
   end
 
   # Check Certificate file for iPhone/Apple Distribution in the KeyChain
+  # @param days: limit days
+  # @return:
+  #   expire_datetime: deadline
+  #   limit_days: limit days
+  #   cname: CN
   def self.certificate_distribution_list_limit_days_for(days:)
     raise "do not set days param" if days.nil?
-    limit_days_for(days: days, type: "certificate_distribution")
+    filtering_limit_days_for(list: certificate_distribution_list.uniq, days: days)
   end
 
   # Check Provisioning Profiles in the Directory that is ~/Library/MobileDevice/Provisioning Profiles/
+  # @param days: limit days
+  # @return:
+  #   expire_datetime: deadline
+  #   limit_days: limit days
+  #   app_identifier: Bundle Identifier
+  #   app_id_name => App ID Name
   def self.provisioning_profile_list_limit_days_for(days:)
     raise "do not set days param" if days.nil?
-    limit_days_for(days: days, type: "provisioning_profile")
+    filtering_limit_days_for(list: provisioning_profile_list.uniq, days: days)
   end
 
+  # All iPhone Developer and Apple Development List
   def self.certificate_development_list
     list = []
     iphone_list = certificate_list_for(name: "iPhone Developer")
     apple_list = certificate_list_for(name: "Apple Development")
     list.concat(iphone_list)
     list.concat(apple_list)
-    return list.uniq
+    return list
   end
 
+  # All iPhone Distribution and Apple Distribution List
   def self.certificate_distribution_list
     list = []
     iphone_list = certificate_list_for(name: "iPhone Distribution")
     apple_list = certificate_list_for(name: "Apple Distribution")
     list.concat(iphone_list)
     list.concat(apple_list)
-    return list.uniq
+    return list
   end
 
-  def self.certificate_info_for(name:)
-    raise "do not set name param" if name.nil?
-
-    temp_pem_file = Tempfile.new(::File.basename("temp_pem"))
-
-    begin
-      `security find-certificate -a -c "#{name}" -p > #{temp_pem_file.path}`
-      result = `openssl x509 -text -fingerprint -noout -in #{temp_pem_file.path}`
-
-      expire_datetime_match = result.match(/.*Not After :(.*)/)
-      raise "not exits expire date" if expire_datetime_match.nil?
-
-      expire_datetime = Time.parse(expire_datetime_match[1])
-
-      cname_match = result.match(/Subject: .* CN=(.*), OU=.*/)
-      raise "not exists cname：#{result}" if cname_match.nil?
-      cname = cname_match[1]
-
-      limit_days = calc_limit_days(datetime: expire_datetime)
-
-    rescue StandardError => e
-      raise(e.message)
-    ensure
-      temp_pem_file.close && temp_pem_file.unlink
-    end
-
-    return {
-        :expire_datetime => expire_datetime,
-        :limit_days => limit_days,
-        :cname => cname
-    }
-  end
-
-  def self.provisioning_profile_list_info(dir: "~/Library/MobileDevice/Provisioning\\ Profiles/*.mobileprovision")
-    info = []
+  # Provisioning Profile List
+  def self.provisioning_profile_list(dir: "~/Library/MobileDevice/Provisioning\\ Profiles/*.mobileprovision")
+    list = []
     Dir.glob("#{File.expand_path(dir)}") do |file|
       file_name_match =  file.match(/.*\/(.*)\.mobileprovision/)
       raise "not exists Provisioning Profile" if file_name_match.nil?
@@ -110,28 +95,72 @@ module AppleCertsInfo
         temp_plist_file.close && temp_plist_file.unlink
       end
 
-      info << {
+      list << {
           :expire_datetime => expire_datetime,
           :limit_days => limit_days,
           :app_identifier => app_identifier,
           :app_id_name => app_id_name
       }
     end
+    return list
+  end
+
+  # Certificate Information for target name
+  # @return
+  #   expire_datetime: deadline
+  #   limit_days: limit days
+  #   cname: CN
+  def self.certificate_info_for(name:)
+    raise "do not set name param" if name.nil?
+
+    info = []
+    begin
+      temp_pem_file = certificate_exchange_pem_file_for(name: name)
+      result = `openssl crl2pkcs7 -nocrl -certfile #{temp_pem_file.path} | openssl pkcs7 -print_certs -text -noout`
+
+      expire_datetime_match = result.scan(/.*Not After :(.*)/)
+      raise "not exits expire date" if expire_datetime_match.nil?
+
+      cname_match = result.match(/Subject: .* CN=(.*), OU=.*/)
+      raise "not exists cname：#{result}" if cname_match.nil?
+
+      expire_datetime_match.each do |original_datetime|
+        expire_datetime = Time.parse(original_datetime.first)
+        limit_days = calc_limit_days(datetime: expire_datetime)
+        cname = cname_match[1]
+
+        info << {
+          :expire_datetime => expire_datetime,
+          :limit_days => limit_days,
+          :cname => cname
+        }
+      end
+    rescue StandardError => e
+      raise(e.message)
+    ensure
+      temp_pem_file.close && temp_pem_file.unlink
+    end
+
     return info
   end
 
-  private
-  def self.limit_days_for(days:, type:)
-    case type
-    when "certificate_development" then
-      list = certificate_development_list
-    when "certificate_distribution" then
-      list = certificate_distribution_list
-    when "provisioning_profile" then
-      list = provisioning_profile_list_info
-    end
-    puts(list) if @debug_log == true
 
+  private
+  def self.certificate_list_for(name:)
+    result = `security find-certificate -a -c "#{name}"`
+    name_match_list = result.scan(/.*alis".*=\"(.*)\".*/)
+    puts(name_match_list) if @debug_log == true
+
+    info = []
+    name_match_list.uniq.each do|name_match|
+      info << certificate_info_for(name:name_match[0])
+    end
+
+    info.flatten!
+  end
+
+  # filtering list
+  def self.filtering_limit_days_for(list:, days:)
     danger_list = []
     list.each do |info|
       danger_list << info if info[:limit_days].to_i <= days.to_i
@@ -140,17 +169,13 @@ module AppleCertsInfo
     danger_list
   end
 
-  def self.certificate_list_for(name:)
-    result = `security find-certificate -a -c "#{name}"`
-    name_match_list = result.scan(/.*alis".*=\"(.*)\".*/)
-    puts(name_match_list) if @debug_log == true
+  # exchange pem file
+  # @param name: unique name
+  def self.certificate_exchange_pem_file_for(name:)
+    temp_pem_file = Tempfile.new(::File.basename("temp_pem"))
+    `security find-certificate -a -c "#{name}" -p > #{temp_pem_file.path}`
 
-    info = []
-    name_match_list.each do|name_match|
-      info << certificate_info_for(name:name_match[0])
-    end
-
-    info
+    temp_pem_file
   end
 
   def self.calc_limit_days(datetime:)
