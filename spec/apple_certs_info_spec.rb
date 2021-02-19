701e63f8e8e1df9bc2948254680d0cc1b2e21c53
tarappo <tarappo@gmail.com>
2021-02-19 16:56:07 +0900

    update test code for using local

--- spec/apple_certs_info_spec.rb
+++ spec/apple_certs_info_spec.rb
@@ -4,16 +4,32 @@ RSpec.describe AppleCertsInfo do
   end
 
   xit "Provisioning Profile info" do
-    info = AppleCertsInfo.provisioning_profile_list_info
+    info = AppleCertsInfo.provisioning_profile_list
+    puts info
   end
 
-  xit "Certificate info" do
-    info = AppleCertsInfo.certificate_distribution_list
+  xit "Certificate Development info" do
     info = AppleCertsInfo.certificate_development_list
+    puts info
   end
 
-  xit "limit days" do
-    list = AppleCertsInfo.provisioning_profile_list_limit_days_for(days: 55)
-    list = AppleCertsInfo.certificate_development_list_limit_days_for(days: 255)
+  xit "Certificate Distribution info" do
+    info = AppleCertsInfo.certificate_distribution_list
+    puts info
+  end
+
+  xit "limit days for Provisioning Profile" do
+    info = AppleCertsInfo.provisioning_profile_list_limit_days_for(days: 56)
+    puts info
+  end
+
+  xit "limit days for Certificate Development" do
+    info = AppleCertsInfo.certificate_development_list_limit_days_for(days: 55)
+    puts info
+  end
+
+  xit "limit days for Certificate Distribution" do
+    info = AppleCertsInfo.certificate_distribution_list_limit_days_for(days: 21)
+    puts info
   end
 end
